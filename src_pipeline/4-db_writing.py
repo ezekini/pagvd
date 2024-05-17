94529b99ad1abec650e8fe32dca1e1366a3a1308
FGP <fedegonzalezp@gmail.com>
2024-05-17 00:31:51 -0300

    enforce date types and increased logic

--- src_pipeline/4-db_writing.py
+++ src_pipeline/4-db_writing.py
@@ -15,21 +15,25 @@ df_top_product = get_df_from_s3_csv(bucket_name, file_name)
 file_name = f'{s3_const.get("transformed_data_folder")}/computed_top_ctr.csv'
 df_top_ctr = get_df_from_s3_csv(bucket_name, file_name)
 
+# cast date columns as dates
+df_top_ctr["date"] = pd.to_datetime(df_top_ctr["date"], format="%Y-%m-%d").dt.date
+df_top_product["date"] = pd.to_datetime(
+    df_top_product["date"], format="%Y-%m-%d"
+).dt.date
 
 print("Filtering by dates missing in DB")
-yesterday = datetime.now() - timedelta(days=1)
+yesterday = datetime.now().date() - timedelta(days=1)
 
 
 def get_date_from_db_table(schema, table) -> datetime:
-    query_dates = f"""SELECT MAX(date) AS mindate FROM {schema}.{table}"""
+    query_dates = f"""SELECT MAX(date) AS maxdate FROM {schema}.{table}"""
     engine = create_db_connection()
     with engine.connect() as connection:
         result = connection.execute(text(query_dates))
-        min_date = result.fetchone()[0]
-        min_date = pd.to_datetime(min_date)
-    if min_date is None:  # return an old date if table is empty
-        min_date = datetime.strptime("2024-04-01", "%Y-%m-%d")
-    return min_date
+        max_date = result.fetchone()[0]
+    if max_date is None:  # return an old date if table is empty
+        max_date = datetime.strptime("2024-04-01", "%Y-%m-%d").date()
+    return max_date
 
 
 last_db_date_top_ctr = get_date_from_db_table(
@@ -38,9 +42,6 @@ last_db_date_top_ctr = get_date_from_db_table(
 last_db_date_top_product = get_date_from_db_table(
     table=db_const.get("top_product_table_name"), schema=db_const.get("schema")
 )
-
-df_top_ctr["date"] = pd.to_datetime(df_top_ctr["date"], format="%Y-%m-%d")
-df_top_product["date"] = pd.to_datetime(df_top_product["date"], format="%Y-%m-%d")
 
 filtered_df_top_ctr = df_top_ctr[
     (df_top_ctr["date"] > last_db_date_top_ctr) & (df_top_ctr["date"] <= yesterday)
@@ -52,16 +53,18 @@ filtered_df_top_product = df_top_product[
 
 
 print("Inserting into DB")
-insert_df_to_db(
-    table_name=db_const.get("top_ctr_table_name"),
-    schema=db_const.get("schema"),
-    data=filtered_df_top_ctr,
-)
-insert_df_to_db(
-    table_name=db_const.get("top_product_table_name"),
-    schema=db_const.get("schema"),
-    data=filtered_df_top_product,
-)
+if not filtered_df_top_ctr.empty:
+    insert_df_to_db(
+        table_name=db_const.get("top_ctr_table_name"),
+        schema=db_const.get("schema"),
+        data=filtered_df_top_ctr,
+    )
+if not filtered_df_top_product.empty:
+    insert_df_to_db(
+        table_name=db_const.get("top_product_table_name"),
+        schema=db_const.get("schema"),
+        data=filtered_df_top_product,
+    )
 
 
 print("Cleaning DB")
